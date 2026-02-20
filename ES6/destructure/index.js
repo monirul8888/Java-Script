ccfede12a45a0b5e2105fcbf9ef3786854fb6633
monirul8888 <monirul429588@gmail.com>
2026-02-20 16:56:52 +0600

    Added Array Destructure

--- ES6/destructure/index.js
+++ ES6/destructure/index.js
@@ -10,3 +10,11 @@ console.log(studentName);
 const { dept,id , name} = student;
 console.log(id);
 console.log(name);
+
+
+const gub = ["GUB", "CSE", "221002154"];
+
+const[university, deptt, idd] = gub;
+console.log(university);
+console.log(idd);
+console.log(deptt);
