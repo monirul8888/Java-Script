d4313382ebfee4653c39226b2dc45be8b037c3f0
monirul8888 <monirul429588@gmail.com>
2026-02-20 15:25:13 +0600

    welcome to ES6

--- ES6/function/index.js
+++ ES6/function/index.js
@@ -1,5 +1,7 @@
-function sum(num1, num2){
+function sum(num1=0, num2=0){
     const total = num1 + num2;
+    console.log("Number 1 = ", num1);
+    console.log("Number 2 = ", num2);
     console.log(total);
 }
-sum(10,20);
+sum(10);
