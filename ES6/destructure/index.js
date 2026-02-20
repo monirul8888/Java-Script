12816704d9acc5f3414794a9a0750579f192ee30
monirul8888 <monirul429588@gmail.com>
2026-02-20 16:52:22 +0600

    Added Desturcre

---
+++ ES6/destructure/index.js
@@ -0,0 +1,12 @@
+const student ={
+    name : "Monirul",
+    id : 221002154,
+    dept : "CSE"
+}
+
+const studentName = student.name;
+console.log(studentName);
+
+const { dept,id , name} = student;
+console.log(id);
+console.log(name);
