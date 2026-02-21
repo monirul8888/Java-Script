187e875e9fd2e12b8ebff2e92a0244e7d9c6dbb7
monirul8888 <monirul429588@gmail.com>
2026-02-21 19:39:05 +0600

    JSON stringify

---
+++ ES6/json/index.js
@@ -0,0 +1,11 @@
+person = {
+    name : "Monirul Islam",
+    id : 221,
+    dept : "CSE"
+}
+
+console.log("Person Object = ", person);
+
+newPerson = JSON.stringify(person);
+
+console.log("JSON File = ", newPerson);
