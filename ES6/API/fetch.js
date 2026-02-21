8de5f6ec7cf6a49f85ead52cc56cf424baadc7bf
monirul8888 <monirul429588@gmail.com>
2026-02-21 21:43:53 +0600

    Fetch data

--- ES6/API/fetch.js
+++ ES6/API/fetch.js
@@ -1,8 +1,4 @@
-const handleLoadData = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(json => console.log(json));
-};
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then(res => res.json())
+    .then(data => console.log(data));
 
-document.getElementById("load-btn")
-        .addEventListener("click", handleLoadData);
--- ES6/json/index.js
+++ ES6/json/index.js
@@ -11,3 +11,8 @@ newPerson = JSON.stringify(person);
 console.log("JSON File = ", newPerson);
 
 console.log("JSON type ", typeof newPerson);
+
+
+newPerson1 = JSON.parse(newPerson);
+console.log("JSON to Object : ", newPerson1);
+console.log("Type : ", typeof newPerson1);
