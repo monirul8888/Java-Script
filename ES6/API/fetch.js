574692c34ce00888f2f6aa20df6e35d18ade3113
monirul8888 <monirul429588@gmail.com>
2026-02-21 22:00:55 +0600

    fetch user

--- ES6/API/fetch.js
+++ ES6/API/fetch.js
@@ -1,4 +1,16 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
+
+
+const loadData = () =>{
+    
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
     .then(res => res.json())
     .then(data => console.log(data));
 
+
+}
+
+const loadUser = () =>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => console.log(data[0]));
+}
